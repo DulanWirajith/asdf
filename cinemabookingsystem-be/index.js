6c00c7595b538508015009b5a17e44ae7cbca973
Dulan (Wirajith) Lokunarangodage <dulanwirajith1995@gmail.com>
2025-01-10 22:34:55 +0800

    add socket

--- cinemabookingsystem-be/index.js
+++ cinemabookingsystem-be/index.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/user-routes.js";
 import adminRouter from "./routes/admin-routes.js";
 import movieRouter from "./routes/movie-routes.js";
 import bookingRoutes from "./routes/booking-routes.js";
+import { Server } from "socket.io";
+import http from "http";
+
 dotenv.config();
 
 const app = express();
@@ -22,10 +25,28 @@ app.use("/admin", adminRouter);
 app.use("/movie", movieRouter);
 app.use("/booking", bookingRoutes);
 
-//Create Responce
 app.get("/", (req, res) => [res.send("Hello Node API")]);
 
-//Connect the Database
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("newBooking", (data) => {
+    console.log("New booking received:", data);
+    io.emit("updateBookings", data); // Broadcast to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 mongoose
   .connect(
     "mongodb+srv://kalanisathyangi:Fmm8ncu3xBlRdBig@cinema-booking-system-d.1ahkq.mongodb.net/Cinema-Booking-System?retryWrites=true&w=majority&appName=Cinema-booking-system-DB"
