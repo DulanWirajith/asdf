e2e55df594d72bfebb48561999fabbfdc14adca2
Dulan (Wirajith) Lokunarangodage <dulanwirajith1995@gmail.com>
2025-01-11 20:20:38 +0800

    coming soon section styles

--- cinemabookingsystem-fe/src/Components/ComingSoonSection.jsx
+++ cinemabookingsystem-fe/src/Components/ComingSoonSection.jsx
@@ -2,23 +2,21 @@ import style from "./comingsoonsection.module.css";
 
 export default function ComingSoonSection({ data: carouselData }) {
   return (
-    <div>
-      <div className={style.nowshowing}>
-        <h1>COMING SOON</h1>
-        <div className={style.imgecontainer}>
-          {carouselData.map((movies, index) => {
-            return (
-              <div key={index} className={style.card}>
-                <img
-                  src={movies.src}
-                  alt={movies.alt}
-                  className={style.cardImg}
-                />
-                <button className={style.button} onClick={null}>Buy Tickets</button>
-              </div>
-            );
-          })}
-        </div>
+    <div className={style.comingSoonContainer}>
+      <h1 className={style.heading}>COMING SOON</h1>
+      <div className={style.imgecontainer}>
+        {carouselData.map((movies, index) => (
+          <div key={index} className={style.card}>
+            <img
+              src={movies.src}
+              alt={movies.alt || "Coming Soon Movie"}
+              className={style.cardImg}
+            />
+            <button className={style.button} onClick={null}>
+              Buy Tickets
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
