43a447aa6411c611ffed56ba9751a04120997961
Dulan (Wirajith) Lokunarangodage <dulanwirajith1995@gmail.com>
2025-01-11 18:17:43 +0800

    sending mail

--- cinemabookingsystem-be/controllers/booking-controller.js
+++ cinemabookingsystem-be/controllers/booking-controller.js
@@ -3,6 +3,20 @@ import Bookings from "../models/Bookings-model.js";
 import Movie from "../models/Movie-model.js";
 import User from "../models/User-model.js";
 import io from "./../index.js"; 
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const CLIENT_ID = process.env.EMAIL_CLIENT_ID;
+const CLIENT_SECRET = process.env.EMAIL_CLIENT_SECRET;
+const REDIRECT_URI = process.env.EMAIL_REDIRECT_URI;
+const REFRESH_TOKEN = process.env.EMAIL_REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 export const newBooking = async (req, res, next) => {
   const { movie, date, time, seatNumber, user } = req.body;
@@ -18,7 +32,7 @@ export const newBooking = async (req, res, next) => {
   if (!existingMovie) {
     return res.status(404).json({ message: "Movie Not Found With Given ID" });
   }
-  if (!user) {
+  if (!existingUser) {
     return res.status(404).json({ message: "User not found with given ID " });
   }
 
@@ -54,6 +68,34 @@ export const newBooking = async (req, res, next) => {
     user,
     date,
     bookingId: booking._id,
+  });
+
+  const accessToken = await oAuth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: existingUser.email,
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+
+  const mailOptions = {
+    from: existingUser.email,
+    to: existingUser.email,
+    subject: "Booking Confirmation",
+    text: `Dear ${existingUser.name},\n\nYour booking for "${existingMovie.title}" on ${date} at ${time} with seat number ${seatNumber} has been confirmed.\n\nThank you for booking with us!`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log("Error sending email: ", error);
+    }
+    console.log("Email sent: " + info.response);
   });
 
   return res.status(201).json({ booking });
